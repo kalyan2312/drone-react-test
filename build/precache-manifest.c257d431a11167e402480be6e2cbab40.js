07f3dbf617c6d1f351a5e00383d4b0f081fe31da
Aabhusan <aabhusan@pop-os.localdomain>
2020-08-05 13:10:58 +0545

    build file added

---
+++ build/precache-manifest.c257d431a11167e402480be6e2cbab40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3acbb76f632ab15cd261fd6c8762ac09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f890978bac6c2d77e6a8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1c3f42eec5b333c2955",
+    "url": "/static/js/2.33c93fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33c93fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f890978bac6c2d77e6a8",
+    "url": "/static/js/main.de9e54a4.chunk.js"
+  },
+  {
+    "revision": "99a5e8f225a170d91a60",
+    "url": "/static/js/runtime-main.df90791b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
